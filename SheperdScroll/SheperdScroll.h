89f4a05cfd3d3f626f7c68cc0bb8c044bd75ae91
Victor Magalhães <ilegra.victor@macbook-pro-de-ilegra.local>
2018-07-26 11:22:09 -0300

    Initial Commit

---
+++ SheperdScroll/SheperdScroll.h
@@ -0,0 +1,19 @@
+//
+//  SheperdScroll.h
+//  SheperdScroll
+//
+//  Created by Victor Panitz Magalhães on 26/07/2018.
+//  Copyright © 2018 Victor Panitz Magalhães. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SheperdScroll.
+FOUNDATION_EXPORT double SheperdScrollVersionNumber;
+
+//! Project version string for SheperdScroll.
+FOUNDATION_EXPORT const unsigned char SheperdScrollVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SheperdScroll/PublicHeader.h>
+
+
